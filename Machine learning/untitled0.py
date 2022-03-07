6dfe4502f37a444336e919ea26c9f5e282645284
Md Salimul Haq Roni <mahedi.rony@gmail.com>
2022-03-08 02:27:09 +0600

    LinearRegression

---
+++ Machine learning/untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 01:25:50 2022
+
+@author: mahed
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df= pd.read_csv("dhaka homeprices.csv")
+print(df)
+df.head(3)
+df.shape
